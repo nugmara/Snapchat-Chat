9b99474fc7dec709c0121b101d81da0622f85a6e
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-19 13:13:20 +0200

    Add form for the messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import io from "socket.io-client";
+import { useState } from "react";
 
 const socket = io("/");
 
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(message)
+  }
+  
   return (
-    <div>Hello World</div>
+    <div>
+
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Write your message..." onChange={(e) => setMessage(e.target.value)}/>
+      <button>Send</button>
+    </form>
+
+    </div>
   )
 }
 
