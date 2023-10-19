7a720b36707b57116e94b23f3872674fa19a176d
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-19 13:07:56 +0200

    Link thr server with the client by proxy

--- server/index.js
+++ server/index.js
@@ -6,5 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
+io.on("connection", socket => {
+    console.log("Client connected")
+})
+
 server.listen(3000)
 console.log("Server on port", 3000)
