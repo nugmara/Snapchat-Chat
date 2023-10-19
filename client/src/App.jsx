7a2cc7a346e59e07b333a9887e85728c3df701d7
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-19 14:37:45 +0200

    Display message on both scree

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,25 @@ const socket = io("/");
 
 function App() {
   const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    socket.on("message", message => {
-      console.log(message)
-    }) 
-  })
+  const [messages, setMessages] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setMessages([...messages, message])
     socket.emit("message", message);
   }
+
+  useEffect(() => {
+    socket.on("message", receiveMessage) 
+
+    return () => {
+      socket.off("message", receiveMessage)
+    }
+  }, [])
+
+  const receiveMessage = message => 
+    setMessages((state) => [...state, message])
+
 
   return (
     <div>
@@ -25,7 +33,13 @@ function App() {
       <input type="text" placeholder="Write your message..." onChange={(e) => setMessage(e.target.value)}/>
       <button>Send</button>
     </form>
-
+    <ul>
+      
+        {messages.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      
+    </ul>
     </div>
   )
 }
