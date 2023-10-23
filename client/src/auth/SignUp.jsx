a83885f9dbc5f5d841808e9fc5212538bd7f68c4
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-23 17:12:04 +0200

    Add styles with tailwind to signup page

--- client/src/auth/SignUp.jsx
+++ client/src/auth/SignUp.jsx
@@ -3,9 +3,45 @@ function SignUp() {
     <section className="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
       <div className="container h-full p-10">
         <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
-            <div className="w-full">
+          <div className="w-full">
+            <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+              <div className="g-0 lg:flex lg:flex-wrap">
+                {/* Left column container */}
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div className="md:mx-6 md:p-12">
+                    {/* Logo */}
+                    <div className="text-center">
+                      <img
+                        className="mx-auto w-48"
+                        src="../../public/snapchat.svg"
+                        alt=""
+                      />
+                      <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
+                        Phantom Pals
+                      </h4>
+                    </div>
 
+                    <form>
+                      <p className="mb-4">Please log in to your account</p>
+                      {/* Username to your account */}
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+                        <input
+                          type="text"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Username"
+                        />
+                        <label htmlFor="exampleFormControlInput1"
+                        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
+
+                        </label>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
       </div>
 
