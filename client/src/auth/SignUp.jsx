f79151c65002933bf04ed7d9cdfb8687bb7d0705
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-23 16:54:10 +0200

    Install tailwind on client folder and start to set up the sign up page

--- client/src/auth/SignUp.jsx
+++ client/src/auth/SignUp.jsx
@@ -1,16 +1,29 @@
-
 function SignUp() {
   return (
-    <div>
-        
-        <form>
-            <input type="text" placeholder="Enter yout username" onChange={(e) => e.target.value} />
-            <input type="password" placeholder="Enter your password" onChange={(e) => e.target.value}/>
-            <button>Sign in!</button>
-        </form>
-    
-    </div>
-  )
+    <section className="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
+      <div className="container h-full p-10">
+        <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div className="w-full">
+
+            </div>
+        </div>
+      </div>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Enter yout username"
+          onChange={(e) => e.target.value}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => e.target.value}
+        />
+        <button>Sign in!</button>
+      </form>
+    </section>
+  );
 }
 
-export default SignUp
+export default SignUp;
