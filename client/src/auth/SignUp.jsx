ecf99b06ac3d19e992c43b0fe2af058feac5ea09
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-24 14:14:33 +0200

    Add styles for form sign up

--- client/src/auth/SignUp.jsx
+++ client/src/auth/SignUp.jsx
@@ -13,7 +13,7 @@ function SignUp() {
                     <div className="text-center">
                       <img
                         className="mx-auto w-48"
-                        src="../../public/snapchat.svg"
+                        src="../../snapchat.svg"
                         alt=""
                       />
                       <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
@@ -30,11 +30,36 @@ function SignUp() {
                           className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleFormControlInput1"
                           placeholder="Username"
+                          onChange={(e) => e.target.value}
                         />
-                        <label htmlFor="exampleFormControlInput1"
-                        className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-
+                        <label
+                          htmlFor="exampleFormControlInput1"
+                          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text"
+                        >
+                          Username
                         </label>
+                      </div>
+                      {/* Password Input */}
+                      <div className="relative mb-4" data-te-input-wrapper-init>
+                        <input
+                          type="password"
+                          className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not(data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          id="exampleFormControlInput1"
+                          placeholder="Password"
+                        />
+                        <label
+                          htmlFor="exampleFormControlInput1"
+                          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                        >
+                          Password
+                        </label>
+                      </div>
+
+                      {/* Submit button */}
+                      <div className="mb-12 pb-1 pt-1 text-center">
+                        <button className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out">
+
+                        </button>
                       </div>
                     </form>
                   </div>
@@ -44,20 +69,6 @@ function SignUp() {
           </div>
         </div>
       </div>
-
-      <form>
-        <input
-          type="text"
-          placeholder="Enter yout username"
-          onChange={(e) => e.target.value}
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          onChange={(e) => e.target.value}
-        />
-        <button>Sign in!</button>
-      </form>
     </section>
   );
 }
