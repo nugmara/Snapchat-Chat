e12ab99c516b42b35ce13df720e110e2c141c27f
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-25 13:51:10 +0200

    Add gradient background to home and commented sign up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
-
 function Home() {
   return (
-    <div className="text-black">Home</div>
-  )
+    <div className="h-screen flex items-center justify-center align-top bg-[conic-gradient(var(--tw-gradient-stops))] from-neutral-400 via-stone-700 to-neutral-900 ">
+      <p className="text-xl text-white text-center absolute inset-x-0 top-6 tracking-widest">
+        A <br /> Ghost <br /> Chat <br /> —
+      </p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
