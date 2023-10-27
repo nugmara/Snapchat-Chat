f53ef2fafe3792b3491b066c2f38ab6cc555088e
Dagmara Grabowska <dagmarag0@gmail.com>
2023-10-27 14:30:41 +0200

    Rutas y lógica para crear el usuario en teoría implementadas

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = new SocketServer(server, {
     connectionStateRecovery: true
 });
 
- dotenv.config()
+dotenv.config()
 // const db = createClient({
 //     url: "libsql://humble-master-chief-nugmara.turso.io",
 //     authToken: process.env.DB_TOKEN
@@ -23,6 +23,44 @@ const io = new SocketServer(server, {
 
 //     }
 // `)
+
+// Routes to create an new user
+app.post("/register", async(req, res) => {
+    const { username, password } = req.body;
+
+    // Validar que los campos no estén vacíos
+    if (!username || !password) {
+        return res
+            .status(400).json({errorMessage: "All selected fields should not be empty"})
+    }
+
+    // Validar que la contraseña sea lo suficientemente fuerte
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    if (passwordRegex === false) {
+        return res.status(400).json({errorMessge: "The password should have at leasst 8 chars, one uppercase, one lowercase and one special character"})
+    }
+
+    try {
+        // Validar si el usuario existe ya
+        const existingUser = await db.query("SELECT * FROM users WHERE username = ?", [username])
+
+        if (existingUser > 0) {
+            return res.status(400).json({message: "El usuario ya existe"});
+        }
+
+        // Encriptar contraseña
+        const hashedPassword = bcrypt.hash(password, 10)
+
+        // Insertar el nuevo usuario en la base de datos 
+        await db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashedPassword]);
+
+        res.status(201).json({message: "Usuario regustrado con éxito"})
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 
 io.on("connection", socket => {
     console.log("Client connected")
